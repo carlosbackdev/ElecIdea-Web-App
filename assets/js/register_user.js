94b36c01b70deae3274c73708d00dfa835731350
carlos <carloslospinos@gmail.com>
2025-01-02 19:10:21 +0100

    crear usuario finalizado

--- assets/js/register_user.js
+++ assets/js/register_user.js
@@ -1,26 +1,70 @@
+
 document.getElementById("menu-toggle").addEventListener("click", function () {
     const menu = document.getElementById("menu");
     menu.classList.toggle("show");
 });
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('registerForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        var password = document.getElementById("password").value;
-        var confirmPassword = document.getElementById("confirmPassword").value;
-        if (password.length < 8 || confirmPassword.length < 8) {  
-            const messageBox = document.getElementById('messageBox');
-            messageBox.textContent = 'la contraseña debe tener al menos 8 caracteres';
-            messageBox.className = 'message-box error';
-            messageBox.style.display = 'block';
-            return;
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const usuario = document.getElementById('usuario').value;
+    const name = document.getElementById('name').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value.trim();
+    const companyCode = document.getElementById('companyCode').value.trim();
+
+    const messageBox = document.getElementById('messageBox');
+    if (password.length < 8 || confirmPassword.length < 8) {
+        messageBox.textContent = 'La contraseña debe tener al menos 8 caracteres.';
+        messageBox.className = 'message-box error';
+        messageBox.style.display = 'block';
+        return;
+    }
+    if (password !== confirmPassword) {
+        messageBox.textContent = 'Las contraseñas no coinciden.';
+        messageBox.className = 'message-box error';
+        messageBox.style.display = 'block';
+        return;
+    }
+    
+    const user  = {
+        usuario: usuario,
+        name: name,
+        password: password
+    };
+
+    fetch('http://localhost:8080/api/user/register?companyCode=' + companyCode, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const messageBox = document.getElementById('messageBox');
+        if (data.status === 'success') {
+            messageBox.textContent = data.message;
+            messageBox.className = 'message-box success';
+            
+            setTimeout(() => {
+                window.location.href = 'file:///A:/PROGRAMACION/ElecIdea/index.html'; // Cambia esta URL según la página a la que quieras redirigir
+            }, 3000); 
+
+        } else {
+        messageBox.textContent = data.message;
+        messageBox.className = 'message-box error'
         }
-        if (password !== confirmPassword) {
-            const messageBox = document.getElementById('messageBox');
-            messageBox.textContent = 'las contraseñas no coinciden';
-            messageBox.className = 'message-box error';
-            messageBox.style.display = 'block';
-            return;
-        }
+        messageBox.style.display = 'block';
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        const messageBox = document.getElementById('messageBox');
+        messageBox.textContent = 'Error al enviar la solicitud. Inténtalo más tarde.';
+        messageBox.className = 'message-box error';
+        messageBox.style.display = 'block';
     });
 });
+
+
+
